d0e569993230df9bcd515e76bb9c2bee1ab0ccd6
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-02-06 16:32:08 +0700

    make theme remain when quit session
    use localStorage to store theme for further session

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,14 +1,26 @@
 import { useState, useContext, createContext } from "react";
 
 const ThemeContext = createContext();
-const isSchemeDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const isThemeDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const getTheme = () => {
+  const localStorageTheme = localStorage.getItem("darkTheme");
+  if (localStorageTheme === "true") {
+    return true;
+  } else if (localStorageTheme === "false") {
+    return false;
+  } else {
+    return isThemeDark;
+  }
+};
 
 export function ThemeProvider({ children }) {
-  const [darkTheme, setDarkTheme] = useState(isSchemeDark);
-  // Set default theme to the system scheme.
+  const [darkTheme, setDarkTheme] = useState(getTheme());
+  // Set default theme to the system theme.
 
   const toggleTheme = () => {
     setDarkTheme((prev) => !prev);
+    localStorage.setItem("darkTheme", !darkTheme ? "true" : "false");
   };
 
   return (
